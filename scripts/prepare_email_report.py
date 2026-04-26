4603f332ae03c565e61929b31eff8e59f47ccd05
Chris Hochgesang <christopherhochgesang@gmail.com>
2026-04-26 16:36:36 +0000

    Email report fix and file saving change

--- scripts/prepare_email_report.py
+++ scripts/prepare_email_report.py
@@ -1,9 +1,10 @@
 import json
 import os
 import sys
+import sqlite3
 from datetime import datetime, timezone, timedelta
 
-def get_category_history(category, limit=20 ,db_path):
+def get_category_history(category,db_path, limit=20 ):
     try:
         conn = sqlite3.connect(db_path)
         c = conn.cursor()
@@ -16,7 +17,8 @@ def get_category_history(category, limit=20 ,db_path):
         return [row[0] for row in reversed(rows)]
     except Exception as e:
         print(f'Warning: Could not get category history for {category}: {e}')
-        return []
+        #return a greater cvss score than is possible so the email doesn't print the category as failing - we can't necessicarily know
+        return [11]
 
     
 def parse_test_results(file_name):
@@ -92,6 +94,7 @@ def main():
 Testing indicates several failing tests of high or critical severity, as defined by CVSS 4.0. <br>
 Please see the <a href = 'https://github.com/openmrs/openmrs-contrib-cvss-scanning'>GitHub repository's</a> tests directory for the tests themselves. <br>"""
     email_text += "\nThe failing tests and their CVSS scores follow:<br><br>\n"
+
     failing_categories =[]
     failing_categories_max_cvss={}
     add_br = False
@@ -110,25 +113,26 @@ Please see the <a href = 'https://github.com/openmrs/openmrs-contrib-cvss-scanni
                 test_file = test["full_name"].split("::")[0].split("/")
                 test_file = test_file[len(test_file)-1]
                 email_text+= f"<b>{test_file}</b> : {test['cvss_score']} <br>\n"
-            else if test['status'] == "FAIL":
+            if test['status'] == "FAIL":
                 if(category not in failing_categories_max_cvss):
                     failing_categories_max_cvss[category]= test['cvss_score']
-                if failing_categories_max_cvss[category] < test['cvss+score']:
-                    failing_categories_max_cvss[category] = test['cvss+score']
+                if failing_categories_max_cvss[category] < test['cvss_score']:
+                    failing_categories_max_cvss[category] = test['cvss_score']
 
     #scan for failing tests with cvss 9.0 or greater, save test file, cvss score
     if(len(failing_categories)==0 and len(failing_categories_max_cvss)==0):
         email_text+="\nNO FAILING TESTS\n"
-    else if (len(failing_categories==0)):
+    elif (len(failing_categories)==0):
         email_text+= "No tests failed with a high or critical score.<br>"
 
     categories_with_score_increase = {}
+
     if(len(failing_categories_max_cvss)>=1):
         #only need to run this if there are failing tests, no failing tests is good and means there aren't 
         try:
-            for category in failing_categories:
+            for category in failing_categories_max_cvss:
                 #get cvss history for category
-                category_history = get_category_history(category, 1,sys.argv[1])[0]
+                category_history = get_category_history(category,sys.argv[2],1)[0]
                 #see if the test historic score is < than the max
                 if(failing_categories_max_cvss[category]>category_history):
                     categories_with_score_increase[category] = failing_categories_max_cvss[category]-category_history
@@ -136,9 +140,12 @@ Please see the <a href = 'https://github.com/openmrs/openmrs-contrib-cvss-scanni
             "ignore category"
 
     if(len(categories_with_score_increase) >1):
-        email_text += "<br> These testing categories saw their highest testing CVSS score increase: <br>"
-        for(category in categories_with_score_increase):
-            email_text += f"<b>{category}</b>: +{categories_with_score_increase[category]}<br>"
+        email_text += "\n<br>These testing categories saw their highest testing CVSS score increase: <br>\n"
+        for category in categories_with_score_increase:
+            email_text += f"<b>{category}</b>: +{categories_with_score_increase[category]}<br>\n"
 
-    print(email_text)
+    #save email to file
+    file = open("email_body.html","w+")
+    file.write(email_text)
+    file.close()
 main()
