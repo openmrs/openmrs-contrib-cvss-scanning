c31af47ea0bebf132205fcd149dac8d55b5cd521
npnorman <npn.not@gmail.com>
2026-05-18 15:36:46 -0400

    cryptographic failures test key is implemented

--- tests/cryptographic_failures/test_should_not_use_the_default_encryption_key.py
+++ tests/cryptographic_failures/test_should_not_use_the_default_encryption_key.py
@@ -1,4 +1,9 @@
+import pytest
 import pytest_bdd
+import docker
+import io
+import tarfile
+import re
 
 from tests.utils import calculate_cvss_v4_score, get_cvss_severity, display_results, BaseMetrics
 from tests.conftest import save_cvss_result
@@ -37,18 +42,56 @@ def test_should_not_use_default_encryption_key():
     pass
 
 @pytest_bdd.given('the default encrpytion key')
-def given():
-    "dTfyELRrAICGDwzjHDjuhw=="
-    pass
+def given_the_default_encryption_key(encryption_data):
+    encryption_data["default_key"] = "dTfyELRrAICGDwzjHDjuhw=="
 
 @pytest_bdd.when('the runtime properties are accessed')
-def when_the_runtime_properties_are_accessed():
-    pass
+def when_the_runtime_properties_are_accessed(encryption_data):
+    
+    client = docker.from_env()
+    container = client.containers.get("openmrs-contrib-cvss-scanning-backend-1")
+    
+    # read in file from docker
+    bits, stat = container.get_archive("/openmrs/data/openmrs-runtime.properties")
+    
+    # convert stream to memory
+    buffer = io.BytesIO()
+    for chunk in bits:
+        buffer.write(chunk)
+    buffer.seek(0)
+    
+    content = ""
+    
+    with tarfile.open(fileobj=buffer) as tar:
+        f = tar.extractfile(tar.getmembers()[0])
+        content = f.read().decode("utf-8")
+    
+    encryption_data["runtime_properties"] = content
 
 @pytest_bdd.when('the encryption key is found')
-def when_the_encryption_key_is_found():
-    pass
+def when_the_encryption_key_is_found(encryption_data):
+    
+    key:str = ""
+    
+    raw:str = encryption_data["runtime_properties"]
+    lines = raw.split('\n')
+    
+    for line in lines:
+        # match encryption.key=
+        
+        if re.search(r"encryption\.key=", line):
+            key = re.split(r"encryption\.key=", line)[1]
+            key = key.replace("\\", "")
+    
+    assert key != ""
+    
+    encryption_data["current_key"] = key
 
 @pytest_bdd.then('the encryption key should not equal to the default key')
-def then_the_encryption_key_should_not_equal_to_the_default_key():
-    assert True
+def then_the_encryption_key_should_not_equal_to_the_default_key(encryption_data):
+    
+    assert encryption_data["current_key"] != encryption_data["default_key"]
+
+@pytest.fixture(scope="function")
+def encryption_data():
+    return {}
