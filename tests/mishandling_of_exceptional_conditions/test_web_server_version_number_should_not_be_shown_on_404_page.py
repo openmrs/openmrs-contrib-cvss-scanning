00b3f487c0d2d34550b97051072801f65c68501a
npnorman <npn.not@gmail.com>
2026-05-26 17:12:10 -0400

    Added test fpr revealing server information

--- tests/mishandling_of_exceptional_conditions/test_web_server_version_number_should_not_be_shown_on_404_page.py
+++ tests/mishandling_of_exceptional_conditions/test_web_server_version_number_should_not_be_shown_on_404_page.py
@@ -1,7 +1,10 @@
+import re
+import pytest
 import pytest_bdd
 
-from tests.utils import calculate_cvss_v4_score, get_cvss_severity, display_results, BaseMetrics
+from tests.utils import calculate_cvss_v4_score, get_cvss_severity, display_results, BaseMetrics, O3_ROOT_URL, DEFAULT_WAIT_TIME
 from tests.conftest import save_cvss_result
+from playwright.sync_api import Page, expect
 
 @pytest_bdd.given('a CVSS score is calculated and printed')
 def given_cvss_score_is_calculted_and_printed(request):
@@ -37,17 +40,26 @@ def test_web_server_version_number_should_not_be_shown_on_404_page():
     pass
 
 @pytest_bdd.given('the attacker visits the page /openmrs/doesnotexist')
-def given_the_attacker_visits_the_page_openmrsdoesnotexist():
-    pass
+def given_the_attacker_visits_the_page_openmrsdoesnotexist(page:Page):
+    page.goto(O3_ROOT_URL + '/doesnotexist')
+    page.wait_for_timeout(DEFAULT_WAIT_TIME)
 
 @pytest_bdd.given('the HTTP status 404 error page is shown')
-def given_the_http_status_404_error_page_is_shown():
-    pass
+def given_the_http_status_404_error_page_is_shown(page:Page):
+    locator = page.get_by_text("HTTP Status 404 \u2013 Not Found")
+    expect(locator).to_be_visible()
 
 @pytest_bdd.when('the attacker looks for the version number')
-def when_the_attacker_looks_for_the_version_number():
-    pass
+def when_the_attacker_looks_for_the_version_number(page:Page, page_data):
+    locator = page.get_by_text(re.compile(r"Apache Tomcat\/\d+\.\d+\.\d+"))
+    
+    page_data["is_text_on_page"] = locator.is_visible()
 
 @pytest_bdd.then('the version number will not be shown')
-def then_the_version_number_will_not_be_shown():
-    pass
+def then_the_version_number_will_not_be_shown(page_data):
+    
+    assert page_data["is_text_on_page"] == False
+
+@pytest.fixture(scope="function")
+def page_data():
+    return {}
