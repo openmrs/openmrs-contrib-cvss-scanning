3ce6dd8c660a8ef588b2b6166e65b167e41db39a
npnorman <npn.not@gmail.com>
2026-08-03 15:12:26 -0400

    added new bar

--- assets/security_dashboard.js
+++ assets/security_dashboard.js
@@ -1,17 +1,21 @@
 
 let details = document.getElementsByClassName("details-category");
 
-//TODO: Set to a button on the screen (on top bar or something)
-document.addEventListener('keydown', (e) => {
-    if (e.key == 'k') {
-        for (i = 0; i < details.length; i++) {
-            details[i].open = false;
-        }
-    } else if (e.key == 'j') {
-        for (i = 0; i < details.length; i++) {
-            details[i].open = true;
-        }
+let openButton = document.getElementById("open-categories");
+let closeButton = document.getElementById("close-categories");
+
+function setDetailsOpen(bool) {
+    for (i = 0; i < details.length; i++) {
+        details[i].open = bool;
     }
+}
+
+openButton.addEventListener('click', () => {
+    setDetailsOpen(true);
+});
+
+closeButton.addEventListener('click', () => {
+    setDetailsOpen(false);
 });
 
 //TODO: OPEN ALL FAILED, OPEN ALL PASSED (subdetails)
